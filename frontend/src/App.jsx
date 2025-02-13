a647a02bbc4f7f690d75b4b17f2633afabca8bf2
sijo07 <clementsijo@gmail.com>
2025-02-13 15:43:50 +0530

    packages installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline font-Raleway ">
+        Hello world!
+      </h1>
+      <Button variant="default">Button</Button>
     </>
   );
 }
